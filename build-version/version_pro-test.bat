20804377bbe658baab16885b4530c5b9989e8c46
xin liu <xinliuchnsz@gmail.com>
2020-03-14 23:34:19 +0800

    add bat error exit code

--- build-version/version_pro-test.bat
+++ build-version/version_pro-test.bat
@@ -1,9 +1,9 @@
-@echo off
-call "%VS100COMNTOOLS%vsvars32.bat"
+@call "%VS100COMNTOOLS%vsvars32.bat"
 
-:: Be careful you cannot using git-bash.exe
-"C:\Program Files\Git\bin\bash.exe" version.sh > version_pro\version_pro\Version.cs
+@rem Be careful you cannot using git-bash.exe
+@"C:\Program Files\Git\bin\bash.exe" version.sh > version_pro\version_pro\Version.cs
+@if not %errorlevel%==0 @exit /B %errorlevel%
 
-devenv version_pro\version_pro\version_pro.csproj /Rebuild "Release"
-version_pro\version_pro\bin\Release\version_pro.exe
+@devenv version_pro\version_pro\version_pro.csproj /Rebuild "Release"
+@version_pro\version_pro\bin\Release\version_pro.exe
 
