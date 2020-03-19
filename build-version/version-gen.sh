5e21327a3c37d0bbc6d3df9426e6d0e9bcbc4285
xin liu <xinliuchnsz@gmail.com>
2020-03-19 22:18:07 +0800

    update version-gen.sh

--- build-version/version-gen.sh
+++ build-version/version-gen.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/bash
 # git describe format: master_v1.0.19-2-g841c059-dirty
-GIT_TAG_INFO=`git describe --dirty`
-GIT_TAG_PARTS=`echo $GIT_TAG_INFO| grep -o - | wc -l`
+tag_count=`git tag | wc -l`
+if [ $tag_count = 0 ]; then
+    echo "please execute 'git tag -a <id>' to specify a git initial commit_id first"
+    exit -1
+fi
 
-# split tag info
-COMMIT_NUM_LAST=`echo $GIT_TAG_INFO | cut -d'-' -f 1 | cut -d'.' -f 3`
-COMMIT_COUNT_NEW=`echo $GIT_TAG_INFO | cut -d'-' -f 2`
-COMMIT_TIME_NEW=`git log --pretty='format:%cd' --date=format:'%Y-%m-%d %H:%M:%S' -n 1`
+tag_describe=`git describe --dirty`
+tag_sections=`echo $tag_describe | grep -o '-' | wc -l`
+ver_last_commit_id=`echo $tag_describe | cut -d '-' -f 1 | cut -d '.' -f 3`
+repo_is_dirty=`echo $tag_describe | grep "dirty$"`
+commit_time=`git log --pretty=format:'%ci' -n 1 | cut -c 1-19 | sed 's/-/\//g'`
 
-# calculate minor version number
-HAS_UNCOMMITTED=`echo $GIT_TAG_INFO | grep "dirty$"`
-if [ -n "$HAS_UNCOMMITTED" ]; then
-    COMMIT_TIME_NEW="MIXED_TIME"
-    COMMIT_NUM_NEW="MIXED_VERSION"
-elif [ $GIT_TAG_PARTS \> 0 ]; then
-    COMMIT_NUM_NEW=$[ $COMMIT_NUM_LAST + $COMMIT_COUNT_NEW ]
+if [ -n "$repo_is_dirty" ]; then
+    commit_time="MIXED_TIME"
+    commit_id="MIXED_VERSION"
+elif [ $tag_sections \> 0 ]; then
+    ver_change_commit_count=`echo $tag_describe | cut -d '-' -f 2`
+    commit_id=$(($ver_last_commit_id + $ver_change_commit_count))
 else
-    #COMMIT_COUNT_NEW=0
-    COMMIT_NUM_NEW=$COMMIT_NUM_LAST
+    #ver_change_commit_count=0
+    commit_id=$ver_last_commit_id
 fi
 
 echo "==="
-echo "Ver: $COMMIT_NUM_NEW"
-echo "Time: $COMMIT_TIME_NEW"
+echo "Ver: $commit_id"
+echo "Time: $commit_time"
 echo "==="
 
 sleep 1s
